0c521cd5bdab037834550059460a530bdbda3d74
Abdullah-C58 <167157159+Abdullah-C58@users.noreply.github.com>
2024-05-28 23:29:48 +0200

    Update BarChart.R
    Ich habe Variablen für die Achsenbeschriftungen verwendet um die Lesbarkeit zu verbessern.
    Ich habe die redundante `barplot()`-Funktion entfernt für einen effizienteren Code.

--- BarChart.R
+++ BarChart.R
@@ -1,22 +1,18 @@
-#barplot(data_xls$Alter)
-
 # Beispiel-Daten: Alter von Personen
 alter <- c(25, 30, 22, 40, 28, 35, 29)
 
 # Berechne den Durchschnitt des Alters
 durchschnitt_alter <- mean(alter)
 
+# Variablen für Achsenbeschriftungen
+x_labels <- c("P1", "P2", "P3", "P4", "P5", "P6", "P7")
+title <- "Alter der Personen"
+x_label <- "Personen"
+y_label <- "Alter"
+
 # Erstelle ein Balkendiagramm mit dem Durchschnitt als Linie
-barplot(alter, names.arg = c("P1", "P2", "P3", "P4", "P5", "P6", "P7"),
-        main = "Alter der Personen",
-        xlab = "Personen", ylab = "Alter",
+barplot(alter, names.arg = x_labels,
+        main = title,
+        xlab = x_label, ylab = y_label,
         col = "skyblue")
 abline(h = durchschnitt_alter, col = "red", lwd = 2)
-
-
-# Erstelle ein Balkendiagramm 
-barplot(alter, names.arg = c("P1", "P2", "P3", "P4", "P5", "P6", "P7"),
-        main = "Alter der Personen",
-        xlab = "Personen", ylab = "Alter",
-        col = "skyblue")
-

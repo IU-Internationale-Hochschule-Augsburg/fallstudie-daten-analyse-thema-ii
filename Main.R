377202332c6c3ddea26abdd0220c160705f3705b
Abdullah-C58 <167157159+Abdullah-C58@users.noreply.github.com>
2024-05-29 00:08:09 +0200

    Update Main.R
    Feature zur Erkennung und Ausgabe des Fehler bei Probleme mit der Excel-Importierung

--- Main.R
+++ Main.R
@@ -1,9 +1,10 @@
 library(readxl)
-# ggplot2 aktivieren
 library(ggplot2)
 
+tryCatch({
+  
 #Excel-Datei in R importieren
-data <- read_excel("C:\\Users\\Jacky\\OneDrive\\Dokumente\\Fallstudien Projekt\\Test Exceltabelle.xlsx",na="NA",)
+data <- read_excel("/Users/abdullahcicek/Desktop/DateiExistiertNicht.xlsx",na="NA",)
 
 age <- subset(data, select = Alter)
 
@@ -15,6 +16,21 @@ ggplot(percent, aes(x="", y=Freq, fill=Var1))+
   geom_label(aes(label=Freq), position=position_stack(vjust=0.5),label.size=0,size=6,show.legend=FALSE)+
   labs(fill="Geschlecht:")+
   theme(legend.text = element_text(size = 15),legend.title = element_text(size = 15))
+
+}, 
+error = function(e) {
+  if (grepl("does not exist", e$message)) {
+    # Fehlerbehandlungsroutine für nicht gefundenen Pfad
+    print("Der angegebene Pfad existiert nicht.")
+  } else {
+    # Allgemeine Fehlerbehandlungsroutine für andere Fehler
+    print("Ein anderer Fehler ist aufgetreten:")
+    print(e)
+  }
+})
+
+
+
 
 
 
